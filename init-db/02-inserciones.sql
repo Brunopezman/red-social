5d1f3447493fe29ce39ccf18ee46819deb8321c6
brunopezman <bpezman@fi.uba.ar>
2025-10-07 16:21:33 -0300

    add compose & .sh, modif dockerfile & modif readme

---
+++ init-db/02-inserciones.sql
@@ -0,0 +1,27 @@
+INSERT INTO Pais (nombre_pais) VALUES
+('Argentina'), ('Colombia'), ('Peru'), ('Chile'), ('México'), ('España'),
+('Uruguay'), ('Paraguay'), ('Bolivia'), ('Venezuela'), ('Brasil'),
+('Nicaragua'), ('Ecuador'), ('Guatemala'), ('Honduras'), ('El Salvador'),
+('Costa Rica'), ('Panamá'), ('República Dominicana');
+
+INSERT INTO Usuarios (id_usuario, username, email, fecha_de_nacimiento, nombre, apellido, pais, cantidad_de_ingresos) VALUES
+(1, 'mjuarez', 'mjuarez@gmail.com', '1990-05-15', 'Martín', 'Juárez', 'Nicaragua', 5),
+(2, 'lcastro', 'lcastro@gmail.com', '1985-03-22', 'Lucía', 'Castro', 'Argentina', 12),
+(3, 'farias', 'farias@gmail.com', '1992-07-10', 'Federico', 'Arias', 'Argentina', 3),
+(4, 'cdominguez', 'cdominguez@gmail.com', '1988-11-30', 'Carla', 'Domínguez', 'Argentina', 8),
+(5, 'agomez', 'agomez@gmail.com', '1995-01-05', 'Agustina', 'Gómez', 'Argentina', 0),
+(6, 'lsanchez', 'lsanchez@gmail.com', '1993-09-17', 'Luciano', 'Sanchez', 'Argentina', 6),
+(7, 'jdasilva', 'jdasilva@gmail.com', '1980-06-25', 'Joao', 'Da Silva', 'Brasil', 15),
+(8, 'rfernandez', 'rfernandez@gmail.com', '1991-04-12', 'Romina', 'Fernández', 'Argentina', 2),
+(9, 'dtorres', 'dtorres@gmail.com', '1987-08-19', 'Diego', 'Torres', 'Argentina', 9),
+(10, 'mramirez', 'mramirez@gmail.com', '1996-12-03', 'María', 'Ramírez', 'Colombia', 1),
+(11, 'ncastillo', 'ncastillo@gmail.com', '1994-02-28', 'Nicolás', 'Castillo', 'Argentina', 4),
+(12, 'fhernandez', 'fhernandez@gmail.com', '1989-10-07', 'Florencia', 'Hernández', 'España', 7),
+(13, 'ealvarez', 'ealvarez@gmail.com', '1990-03-15', 'Ezequiel', 'Álvarez', 'Argentina', 0),
+(14, 'gromero', 'gromero@gmail.com', '1986-07-21', 'Gabriela', 'Romero', 'Paraguay', 11),
+(15, 'psanchez', 'psanchez@gmail.com', '1997-05-09', 'Pablo', 'Sánchez', 'Argentina', 3),
+(16, 'jnavarro', 'jnavarro@gmail.com', '1992-01-30', 'Jimena', 'Navarro', 'Bolivia', 6),
+(17, 'lvaldez', 'lvaldez@gmail.com', '1984-09-14', 'Leandro', 'Valdez', 'Ecuador', 13),
+(18, 'hcastillo', 'hcastillo@gmail.com', '1993-06-18', 'Hernán', 'Castillo', 'Peru', 2),
+(19, 'ysilva', 'ysilva@gmail.com', '1981-12-25', 'Yanina', 'Silva', 'Colombia', 10),
+(20, 'rvera', 'rvera@gmail.com', '1995-08-01', 'Rodrigo', 'Vera', 'Venezuela', 1);
