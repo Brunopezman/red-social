d2e5093628335793e026e10c90f0ca1109ee6885
Bruno Pezman <bpezman@fi.uba.ar>
2026-02-21 17:45:09 -0300

    Changing password for all roles

--- init-db/01-roles.sql
+++ init-db/01-roles.sql
@@ -3,9 +3,9 @@ DROP ROLE IF EXISTS admin_user;
 DROP ROLE IF EXISTS app_user;
 DROP ROLE IF EXISTS read_only_user;
 
-CREATE ROLE admin_user WITH LOGIN PASSWORD 'admin_secret_password';
+CREATE ROLE admin_user WITH LOGIN PASSWORD 'admin123';
 
-CREATE ROLE app_user WITH LOGIN PASSWORD 'app_secure_password';
+CREATE ROLE app_user WITH LOGIN PASSWORD 'user123';
 
-CREATE ROLE read_only_user WITH LOGIN PASSWORD 'read_only_password';
-CREATE ROLE developer WITH LOGIN PASSWORD 'dev_password';
+CREATE ROLE read_only_user WITH LOGIN PASSWORD 'read123';
+CREATE ROLE developer WITH LOGIN PASSWORD 'dev123';
